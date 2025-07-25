75b7cff34ce5c54ab0064069e230ff24e989e3d3
najim2004 <najim10112004@gmail.com>
2025-07-25 12:12:19 +0600

    Add CourseInstructor component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import CourseInfo from "@/components/course-info";
+import CourseInstructor from "@/components/course-instructor";
 
 export default function Home() {
-  return <div className="">
-    <CourseInfo/>
-  </div>;
+  return (
+    <div className="">
+      <CourseInfo />
+      <main className="max-w-[1200px] mx-auto flex flex-col gap-4 md:flex-row md:gap-12 ">
+        <section className="order-2 flex-1 md:order-1  md:max-w-[calc(100%_-_348px)] lg:max-w-[calc(100%_-_448px)]">
+          <CourseInstructor />
+        </section>
+        <section className="w-full md:max-w-[330px] lg:max-w-[400px] order-2 bg-white"></section>
+      </main>
+    </div>
+  );
 }
